d4d517f2d17efef678401eb27d4e31bcd5273138
Ritik2004 <hritikgaur44@gmail.com>
2026-08-18 01:55:02 +0530

    Thrid commit

--- rideservice/demo/src/main/java/com/example/riderservice/demo/service/Rideservice.java
+++ rideservice/demo/src/main/java/com/example/riderservice/demo/service/Rideservice.java
@@ -1,18 +1,34 @@
 package com.example.riderservice.demo.service;
 
+import com.example.driverservice.entity.DriverEntity;
 import com.example.riderservice.demo.entity.RideEntity;
 import com.example.riderservice.demo.repository.RideRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import java.util.List;
 
 @Service
 public class Rideservice {
     private final RideRepository rideRepository;
+    private final RestClient restClient;
 
-    public Rideservice(RideRepository rideRepository){
+    public Rideservice(RideRepository rideRepository,RestClient restClient){
         this.rideRepository=rideRepository;
+        this.restClient=restClient;
     }
-
     public RideEntity createRide(RideEntity rideEntity){
+        //1) Get available rider
+        //2 Assign driver
+        //3 change status
+        //4 save rider
+        List<DriverEntity> drivers = restClient
+                .get()
+                .uri("http://localhost:8081/available")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<DriverEntity>>() {});
+        System.out.println(drivers);
         return rideRepository.save(rideEntity);
     }
 }
